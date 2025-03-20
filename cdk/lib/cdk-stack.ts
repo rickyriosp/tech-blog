29da0badfa8ffbc8d570a314be8d65bbed32f716
rickyriosp <riki.rios91@gmail.com>
2025-03-20 00:32:03 -0400

    cdk update

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -19,25 +19,30 @@ export class CdkStack extends cdk.Stack {
     // ----------------------------------------------------------------------
     const blogBucket = new s3.Bucket(this, 'BlogBucket', {
       bucketName: props.blogUrl,
-      blockPublicAccess: new s3.BlockPublicAccess({ blockPublicAcls: false }),
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }),
       publicReadAccess: true,
       encryption: s3.BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
+      enforceSSL: false,
       versioned: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: '404.html',
     });
 
-    blogBucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        sid: 'PublicGetReadObject',
-        effect: iam.Effect.ALLOW,
-        actions: ['s3:GetObject'],
-        resources: [`arn:aws:s3:::${props.blogUrl}/*`],
-        principals: [new iam.AnyPrincipal()],
-      }),
-    );
+    // blogBucket.addToResourcePolicy(
+    //   new iam.PolicyStatement({
+    //     sid: 'PublicGetReadObject',
+    //     effect: iam.Effect.ALLOW,
+    //     actions: ['s3:GetObject'],
+    //     resources: [`arn:aws:s3:::${props.blogUrl}/*`],
+    //     principals: [new iam.StarPrincipal()],
+    //   }),
+    // );
 
     // ----------------------------------------------------------------------
     // ACM Certificate
@@ -87,57 +92,57 @@ export class CdkStack extends cdk.Stack {
     // ----------------------------------------------------------------------
     // OIDC Provider
     // ----------------------------------------------------------------------
-    // const githubProvider = new iam.OpenIdConnectProvider(this, 'GitHubOidcProvider', {
-    //   url: 'https://token.actions.githubusercontent.com',
-    //   clientIds: ['sts.amazonaws.com'],
-    //   thumbprints: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
-    // });
+    const githubProvider = new iam.OpenIdConnectProvider(this, 'GitHubOidcProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com'],
+      thumbprints: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
+    });
 
     // ----------------------------------------------------------------------
     // IAM Policy
     // ----------------------------------------------------------------------
-    // const policyDocument = new iam.PolicyDocument({
-    //   statements: [
-    //     new iam.PolicyStatement({
-    //       sid: 'GitHubActionsS3',
-    //       effect: iam.Effect.ALLOW,
-    //       actions: ['s3:PutObject', 's3:GetObject', 's3:ListBucket'],
-    //       resources: [`arn:aws:s3:::${props.blogUrl}`, `arn:aws:s3:::${props.blogUrl}/*`],
-    //     }),
-    //     new iam.PolicyStatement({
-    //       sid: 'GitHubActionsCFD',
-    //       effect: iam.Effect.ALLOW,
-    //       actions: ['cloudfront:CreateInvalidation'],
-    //       resources: [
-    //         `arn:aws:cloudfront::${this.account}:distribution/${blogDistribution.distributionId}`,
-    //       ],
-    //     }),
-    //   ],
-    // });
+    const policyDocument = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          sid: 'GitHubActionsS3',
+          effect: iam.Effect.ALLOW,
+          actions: ['s3:PutObject', 's3:GetObject', 's3:ListBucket'],
+          resources: [`arn:aws:s3:::${props.blogUrl}`, `arn:aws:s3:::${props.blogUrl}/*`],
+        }),
+        new iam.PolicyStatement({
+          sid: 'GitHubActionsCFD',
+          effect: iam.Effect.ALLOW,
+          actions: ['cloudfront:CreateInvalidation'],
+          resources: [
+            `arn:aws:cloudfront::${this.account}:distribution/${blogDistribution.distributionId}`,
+          ],
+        }),
+      ],
+    });
 
-    // const githubPolicy = new iam.ManagedPolicy(this, 'GitHubS3Policy', {
-    //   managedPolicyName: 'GitHubS3Policy',
-    //   description:
-    //     'Allow read-write access to the tech-blog S3 bucket, and to invalidate CloudFront cache',
-    //   document: policyDocument,
-    // });
+    const githubPolicy = new iam.ManagedPolicy(this, 'GitHubS3Policy', {
+      managedPolicyName: 'GitHubS3Policy',
+      description:
+        'Allow read-write access to the tech-blog S3 bucket, and to invalidate CloudFront cache',
+      document: policyDocument,
+    });
 
-    // // ----------------------------------------------------------------------
-    // // IAM Role
-    // // ----------------------------------------------------------------------
-    // const githubRole = new iam.Role(this, 'GitHubS3Role', {
-    //   roleName: 'GitHubS3Role',
-    //   description: 'Role used by GitHub Actions to push new content to blog S3 Bucket',
-    //   maxSessionDuration: cdk.Duration.hours(2),
-    //   managedPolicies: [githubPolicy],
-    //   assumedBy: new iam.WebIdentityPrincipal(githubProvider.openIdConnectProviderArn, {
-    //     StringEquals: {
-    //       'token.actions.githubusercontent.com:aud': ['sts.amazonaws.com'],
-    //     },
-    //     StringLike: {
-    //       'token.actions.githubusercontent.com:sub': ['repo:rickyriosp/tech-blog:*'],
-    //     },
-    //   }),
-    // });
+    // ----------------------------------------------------------------------
+    // IAM Role
+    // ----------------------------------------------------------------------
+    const githubRole = new iam.Role(this, 'GitHubS3Role', {
+      roleName: 'GitHubS3Role',
+      description: 'Role used by GitHub Actions to push new content to blog S3 Bucket',
+      maxSessionDuration: cdk.Duration.hours(2),
+      managedPolicies: [githubPolicy],
+      assumedBy: new iam.WebIdentityPrincipal(githubProvider.openIdConnectProviderArn, {
+        StringEquals: {
+          'token.actions.githubusercontent.com:aud': ['sts.amazonaws.com'],
+        },
+        StringLike: {
+          'token.actions.githubusercontent.com:sub': ['repo:rickyriosp/tech-blog:*'],
+        },
+      }),
+    });
   }
 }
